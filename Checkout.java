39497f98ab82922183f8c7960a324ccaadc58048
Eric Truong <pengu@dhcp-39-16-217.dhcpint.csulb.edu>
2020-02-18 15:28:40 -0800

    Working on checkout

---
+++ Checkout.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Checkout extends java.lang.Object {
+
+    private ArrayList<DessertItem> checkOut = new ArrayList<>();
+
+    public Checkout(){
+
+        ArrayList<DessertItem> checkOut = new ArrayList<>();
+    }
+
+    public void clear(){
+
+        checkOut.clear();
+    }
+
+}
