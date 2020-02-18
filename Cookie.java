a89e37185aecdc793579128356c088586af7e605
Eric Truong <pengu@dhcp-39-8-189.dhcpint.csulb.edu>
2020-02-17 17:20:18 -0800

    Worked on Cookie file

--- Cookie.java
+++ Cookie.java
@@ -1,6 +1,16 @@
-public class Cookie {
+public class Cookie extends DessertItem {
 
     private int amount;
     private double per; // price per dozen
     private int cal;
+
+    public Cookie(){
+
+        super();
+    }
+
+    public double getCost(){
+
+        per = Math.round(amount(399/12));
+    }
 }
