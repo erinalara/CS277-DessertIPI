31a26e26c0c5d8fc1e8bf655b7cfe45ecfd5cd97
erinalara <48671321+erinalara@users.noreply.github.com>
2020-02-18 09:49:42 -0800

    Update Cookie.java

--- Cookie.java
+++ Cookie.java
@@ -1,19 +1,40 @@
 public class Cookie extends DessertItem {
 
     private int amount;
-    private double per; // price per dozen
+    private double price; // price per dozen
     private int cal;
 
     public Cookie(String n){
-
         super(n);
+    }
+
+    public Cookie(String n, int a){
+        super(n);
+        amount = a;
+    }
+
+    public void setCal(int c) {
+        cal = c;
+    }
+
+    public int getCal() {
+        return cal;
     }
 
     public double getCost(){
 
-        //per = Math.round(amount(399/12));
+        final double chocoRate = 499 / 12;
+        final double oatRate = 399 / 12;
 
-        return 0.0;
+        if ((name.equals("Chocolate Chip"))) {
+            price = Math.round(amount*(chocoRate));
+        }
+
+        else if ((name.equals("Oatmeal"))){
+            price = Math.round(amount*(oatRate));
+        }
+
+        return price;
     }
 
 
