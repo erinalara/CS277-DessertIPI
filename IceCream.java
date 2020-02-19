6c3da326b6d744cf08b23a6ec3e902bed33b0afe
erinalara <48671321+erinalara@users.noreply.github.com>
2020-02-18 19:01:37 -0800

    Update IceCream.java

--- IceCream.java
+++ IceCream.java
@@ -1,7 +1,40 @@
-public class IceCream {
+public class IceCream extends DessertItem{
 
     private double price;
     private int cal;
 
+    public IceCream(String n){
+        super(n);
+        price = 0.0;
+        cal = 0;
+    }
 
+    public IceCream(String n, int c){
+
+        super(n);
+
+        cal = c;
+    }
+
+    public double getCost(){
+
+        final double choco = 9.99;
+        final double van = 7.00;
+
+        if ((name.equals("Chocolate"))) {
+            price = choco;
+        }
+
+        else if ((name.equals("Vanilla"))){
+            price = van;
+        }
+
+        return price;
+    }
+
+    @Override
+    public String toString(){
+
+        return price + "\n" + name + "(Ice Cream) \t" + getCost();
+    }
 }
