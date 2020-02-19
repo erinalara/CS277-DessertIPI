25a490a67383c3cf6cc37f3344046c98a3dc83e6
erinalara <erinalara888@gmail.com>
2020-02-18 19:16:55 -0800

    i scream for ice cream

--- Tester.java
+++ Tester.java
@@ -6,12 +6,17 @@ public class Tester {
 
         ArrayList<DessertItem> desserts = new ArrayList<>();
 
-        desserts.add(new Candy("Corn", 1.2, 0.89, 100));
-        //desserts.add(new Candy("Lolipop", 2.5));
+        desserts.add(new Candy("Corn", 1.2, 7.25, 500));
+        desserts.add(new Candy("Lollipop", 2.5, 8.25, 640 ));
+        desserts.add(new Cookie("Chocolate Chip", 17, 4.99, 250));
+        desserts.add(new Cookie("Oatmeal", 15, 3.99, 240));
+        desserts.add(new IceCream("Chocolate", 500));
+        desserts.add(new IceCream("Vanilla", 500));
+        desserts.add(new Sundae("Caramel"));
+        desserts.add(new Sundae("Strawberry"));
 
         for (int i = 0; i < desserts.size(); i++) {
             DessertItem tester = desserts.get(i);
-
             System.out.println(tester);
         }
     }
