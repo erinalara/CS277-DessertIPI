aeee0f61b7490396e31a15eb2acf72628374ac3c
erinalara <erinalara888@gmail.com>
2020-02-18 20:06:00 -0800

    i scream for ice cream

---
+++ Checkout.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Checkout {
+
+    private ArrayList<DessertItem> bag;
+
+    public Checkout() {
+        bag = new ArrayList<>();
+    }
+
+    public int numberofItems() {
+        return bag.size();
+    }
+
+    public void enterItem(DessertItem item) {
+        bag.add(item);
+    }
+
+    public void clear() {
+        bag.clear();
+    }
+
+    public int totalCost() {
+        int sum = 0;
+        for (int i = 0; i < bag.size(); i++) {
+            DessertItem item = bag.get(i);
+            sum += item.getCost();
+        }
+        return sum;
+    }
+
+    public int totalTax() { // implement
+        int tax = 0;
+        return tax;
+    }
+
+    @Override
+    public String toString() {
+        String receipt = "";
+        receipt += "    EE! Desserts of Disneyland!     " + '\n' + "    ___________________________     " + '\n';
+        for (int i = 0; i < bag.size(); i++) {
+            DessertItem tester = bag.get(i);
+            receipt += tester;
+            receipt += '\n';
+        }
+
+        receipt += " Tax:       " + totalTax() + '\n' + "Total cost:        " + totalCost();
+        return receipt;
+    }
+}
--- Tester.java
+++ Tester.java
@@ -5,20 +5,26 @@ public class Tester {
     public static void main (String[] args) {
 
         ArrayList<DessertItem> desserts = new ArrayList<>();
+        Checkout cart = new Checkout();
 
-        desserts.add(new Candy("Corn", 1.5, 7.25, 500));
-        desserts.add(new Candy("Lollipop", 2.5, 8.25, 640 ));
-        desserts.add(new Cookie("Chocolate Chip", 17, 4.99, 250));
-        desserts.add(new Cookie("Oatmeal", 15, 3.99, 240));
-        desserts.add(new IceCream("Vanilla", 10.05, 500));
-        desserts.add(new IceCream("Chocolate", 9.99, 500));
-        desserts.add(new Sundae("Caramel", 7.70, 600));
-        desserts.add(new Sundae("Strawberry", 6.90, 560));
 
-        for (int i = 0; i < desserts.size(); i++) {
+        cart.enterItem(new Candy("Corn", 1.5, 7.25, 500));
+        cart.enterItem(new Candy("Lollipop", 2.5, 8.25, 640 ));
+        cart.enterItem(new Cookie("Chocolate Chip", 17, 4.99, 250));
+        cart.enterItem(new Cookie("Oatmeal", 15, 3.99, 240));
+        cart.enterItem(new IceCream("Vanilla", 10.05, 500));
+        cart.enterItem(new IceCream("Chocolate", 9.99, 500));
+        cart.enterItem(new Sundae("Caramel", 7.70, 600));
+        cart.enterItem(new Sundae("Strawberry", 6.90, 560));
+
+        System.out.println(cart.numberofItems());
+        System.out.println(cart.toString());
+
+        /*for (int i = 0; i < desserts.size(); i++) {
             DessertItem tester = desserts.get(i);
             System.out.println(tester);
             System.out.println();
-        }
+        }*/
+
     }
 }
