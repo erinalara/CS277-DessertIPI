2f420cfdc96fd9147cda32a3c04b9208d58d4f79
erinalara <erinalara888@gmail.com>
2020-02-18 19:09:43 -0800

    i scream for ice cream

--- Sundae.java
+++ Sundae.java
@@ -1,7 +1,34 @@
-public class Sundae {
+public class Sundae extends IceCream {
 
     private double price; // cost of ice cream + topping
     private int cal;
 
+    public Sundae(String n) {
+        super(n);
+    }
+
+    public double getCost(){
+
+        price = super.getCost();
+
+        final double carm = 11.99;
+        final double straw = 9.00;
+
+        if ((name.equals("Caramel"))) {
+            price += carm;
+        }
+
+        else if ((name.equals("Strawberry"))){
+            price += straw;
+        }
+
+        return price;
+    }
+
+    @Override
+    public String toString(){
+
+        return price + "\n" + name + "(Sundae) \t" + getCost();
+    }
 
 }
